5b6b384480dfd7e2842627408ab53c0ea0252585
PMGWork <pmgwork.com@gmail.com>
2026-08-26 14:32:03 +0900

    Initial Commit

---
+++ awake/awakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  awakeApp.swift
+//  awake
+//
+//  Created by Yuto Chiba on 2026/08/26.
+//
+
+import SwiftUI
+
+@main
+struct awakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
